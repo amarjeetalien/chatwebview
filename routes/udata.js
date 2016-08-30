b8f852dabc8099dfa86be00d650a80b34830baec
Aamarjeet Kumar <amarjeetalien@gmail.com>
2016-08-30 17:10:33 +0530

    connect to mlab

--- routes/udata.js
+++ routes/udata.js
@@ -2,11 +2,12 @@ var express = require('express')
 var router = express.Router()
 
 var monk = require('monk')
-var db = monk('localhost:27017/KLM_DEMO_DB')
-var collection = db.get('UserDetails')
+var db = monk('klmdbuser:klmadmin@ds147905.mlab.com:47905/klmdemodb')
+var collection = db.get('passenger_details')
 
 router.get('/', function(req, res){
 	collection.find({}, function(err, udata){
+		console.log(udata)
 		if (err) {throw err}
 		console.log(udata)
 		res.json(udata)
