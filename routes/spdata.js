640fac1db4bad9ff3bf3c203fc060cdc89bae952
Aamarjeet Kumar <amarjeetalien@gmail.com>
2016-09-01 13:28:11 +0530

    return one name for demo

--- routes/spdata.js
+++ routes/spdata.js
@@ -6,12 +6,13 @@ var db = monk('klmdbuser:klmadmin@ds147905.mlab.com:47905/klmdemodb')
 var collection = db.get('passenger_details')
 
 router.get('/:sid', function(req, res){
-	collection.find({}, function(err, udata){
+	/*collection.find({}, function(err, udata){
 		console.log(udata)
 		if (err) {throw err}
 		console.log(udata)
 		res.json(udata)
-	})
+	})*/
+	res.json({"names" : ["Mrs. Liona ver der Schoot"]})
 })
 
 module.exports = router
